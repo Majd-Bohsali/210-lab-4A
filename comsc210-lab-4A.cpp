1f2951e610181c131e57efd55cb9346fee1e7e0a
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-09-03 17:36:11 -0700

    Initial commit

---
+++ comsc210-lab-4A.cpp
@@ -0,0 +1,11 @@
+// COMSC-210 | Lab 4A | Majd Bohsali
+// IDE used: Visual Studio Code
+
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+int main() { 
+    cout << "Hello World";
+    return 0;
+}
