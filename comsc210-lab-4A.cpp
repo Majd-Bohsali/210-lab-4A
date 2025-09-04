1f313acb89c0279792a7ca20d778f57421264be9
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-09-03 17:38:14 -0700

    Defined Color Struct

--- comsc210-lab-4A.cpp
+++ comsc210-lab-4A.cpp
@@ -1,9 +1,15 @@
 // COMSC-210 | Lab 4A | Majd Bohsali
 // IDE used: Visual Studio Code
-
 #include <iostream>
 #include <vector>
 using namespace std; 
+
+// defines Color struct
+struct Color {
+    int red;
+    int blue;
+    int green;
+};
 
 int main() { 
     cout << "Hello World";
