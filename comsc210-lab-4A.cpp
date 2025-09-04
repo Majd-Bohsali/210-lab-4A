7ab3cc4f5207432f4c0d359422122d53cd03c6c9
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-09-03 17:55:15 -0700

    Added random color generator

--- comsc210-lab-4A.cpp
+++ comsc210-lab-4A.cpp
@@ -9,21 +9,29 @@ using namespace std;
 // defines Color struct
 struct Color {
     int red;
-    int blue;
     int green;
+    int blue;
 };
 
 int main() { 
     srand(time(0));
 
     const int MIN_N = 25; 
-    const int MAX_N = 25; 
+    const int MAX_N = 50; 
+    const int MAX_COLOR_INT = 256; 
     vector<Color> colors; 
     int n = rand() % (MAX_N - MIN_N + 1) + MIN_N;
 
+    // generates n random colors
     for(int i = 0; i < n; i++) {
-        
+        int redRand = rand() % MAX_COLOR_INT;
+        int blueRand = rand() % MAX_COLOR_INT;
+        int greenRand = rand() % MAX_COLOR_INT;
+        Color tempColor = {redRand, greenRand, blueRand};
+        colors.push_back(tempColor); 
     }
+
+
 
     return 0;
 }
